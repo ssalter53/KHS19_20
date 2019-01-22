7807387434519ce236dea007b6a25b4b630aec64
iiKr4v3u5 <45744930+iiKr4v3u5@users.noreply.github.com>
2019-01-22 11:19:54 -1000

    Update team9.py

--- 1_3_9-10_sourceFiles/team9.py
+++ 1_3_9-10_sourceFiles/team9.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'MckeandJar' # Only 10 chars displayed.
+strategy_name = 'Collude but retaliate twice'
+strategy_description = 'Collude until betrayed, then betray twice. '
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,14 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history) == 0:
+        return 'c'
+    elif my_history[-1]== 'c' and their_history[-1]== 'b':
+        return 'b'
+    elif (len(my_history) > 1) and (my_history[-2]== 'c' and their_history[-2]== 'b'):
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
