f6ab92702c1eaf4699e7453f849803955e7661ef
chrispascual <46632314+chrispascual@users.noreply.github.com>
2019-01-22 11:27:27 -1000

    Update team4.py

--- 1_3_9-10_sourceFiles/team4.py
+++ 1_3_9-10_sourceFiles/team4.py
@@ -6,27 +6,27 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+import random
+import math
+n = random.randint(0, 50)
+team_name = 'Easy Dub'
+strategy_name = 'Collude until Betrayed'
+strategy_description = '''\
+Collude first round. Collude, except in a round after getting 
+a severe punishment.'''  
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    #elif (len(their_history)==10 and their_history[-9]=='b'):
+    #    return 'b'
+    elif n !=0 and their_history[-1]=='b':
+        return 'b'
+        n - 1
+    elif (len(their_history)==10 and their_history[-9]=='b'):
+        return 'b'
+    else:
+        return 'c'
+    print my_history
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
