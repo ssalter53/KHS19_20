ef11fa2e92452e02ec7e0e778b9133a49244e27e
Spunnt <46978501+Spunnt@users.noreply.github.com>
2019-01-23 12:18:18 -1000

    Update team2.py

--- 1_3_9-10_sourceFiles/team2.py
+++ 1_3_9-10_sourceFiles/team2.py
@@ -6,8 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+import random
+
+team_name = ' RYJO' # Only 10 chars displayed.
+strategy_name = 'First 4 rounds random if less than 50% of opponents move is betray, collude'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -17,16 +19,22 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    #b_counter = their_history.count('b')
+    #if their_history < 5: 
+    #    return random.choice('c', 'b')  
+    #else:
+    #    if their_history[-4: -1].count('b')>= 2:
+    #        return 'b'
+    #    else: 
+    #        return 'c'
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    b_counter = their_history.count ('b')
+    if len(their_history)<=3: 
+        return 'c'
+    elif their_history[-1] == 'c' and their_history[-2] == 'c' and their_history[-3] == 'c':
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
