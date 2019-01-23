78afcb7dc73c4e3f66940b10a7818f5230c109f6
Deznel2 <46978581+Deznel2@users.noreply.github.com>
2019-01-23 12:21:17 -1000

    Update team1.py

--- 1_3_9-10_sourceFiles/team1.py
+++ 1_3_9-10_sourceFiles/team1.py
@@ -6,27 +6,39 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+team_name = 'Kameron&Denzel' # Only 10 chars displayed.
+strategy_name = 'Bait&Twitch'
+strategy_description = ''        
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+def move(my_history, their_history, my_score, their_score):    
+    #choice = their_history
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len (their_history) < 5:
+        return "c"
+    elif len (their_history) < 7:
+        return "b"
+    elif len (their_history) == 7:
+        return "c"
+    elif len (their_history) == 8:
+        return "b"
+    elif len (their_history) < 10:
+        return "c"
+    elif len (their_history) < 13:
+        return "b"
+    elif len (their_history) == 14:
+        return"c"
+    elif len (their_history) == 15:
+        return "b"    
+    elif len (their_history) == 16:
+        return "c"
+    elif len (their_history) == 17:
+        return "b"
+    cs = their_history[-35:-1].count ("c")
+    bs = their_history[-35:-1].count ("b")
+    if cs > bs:
+        return "c"
+    else:
+        return "b"
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
